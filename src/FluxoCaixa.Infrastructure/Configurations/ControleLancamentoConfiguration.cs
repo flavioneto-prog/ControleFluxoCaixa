0e1dcac019c744b3ef6c2c62474597f1ef643ec3
Flavio Neto <flavionetodev@outlook.com>
2024-04-30 13:03:05 -0300

    Adicionando configuration para a entidade ControleLancamento, visando a utlização de fluent API.

---
+++ src/FluxoCaixa.Infrastructure/Configurations/ControleLancamentoConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ControleFluxoCaixa.Infrastructure.Configurations;
+
+public class ControleLancamentoConfiguration : IEntityTypeConfiguration<ControleLancamento>
+{
+    public void Configure(EntityTypeBuilder<ControleLancamento> builder)
+    {
+        builder
+            .ToTable("TB_CONTROLE_LANCAMENTO");
+
+        builder
+            .Property(controleLancamento => controleLancamento.Id)
+            .HasColumnType("bigint")
+            .ValueGeneratedOnAdd();
+
+        builder
+            .HasKey(controleLancamento => controleLancamento.Id);
+
+        builder
+            .Property(controleLancamento => controleLancamento.DataLancamento)
+            .HasColumnType("datetime")
+            .HasColumnName("DataLancamento")
+            .IsRequired();
+
+        builder
+            .Property(controleLancamento => controleLancamento.TipoLancamento)
+            .HasColumnType("varchar")
+            .HasColumnName("TipoLancamento")
+            .HasMaxLength(60)
+            .IsRequired();
+
+        builder
+            .Property(controleLancamento => controleLancamento.Cliente)
+            .HasColumnType("varchar")
+            .HasColumnName("Cliente")
+            .HasMaxLength(60)
+            .IsRequired();
+
+        builder
+            .Property(controleLancamento => controleLancamento.Valor)
+            .HasColumnType("decimal")
+            .HasColumnName("Valor")
+            .IsRequired();
+    }
+}
